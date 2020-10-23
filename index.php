c191875de2c5cedb7eb84f3171bf47ea1b2c5289
Álvaro Artano <alvaroartano@gmail.com>
2020-10-23 11:23:38 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+
+$cURL = curl_init();
+$setopt_array = array(CURLOPT_URL => "https://www.digitalocean.com/community/api/random_username",    CURLOPT_RETURNTRANSFER => true, CURLOPT_HTTPHEADER => array()); 
+curl_setopt_array($cURL, $setopt_array);
+$json_response_data = curl_exec($cURL);
+echo $json_response_data);
+curl_close($cURL);  
+?>
